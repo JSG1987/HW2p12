2ae604c9c1b8aaff452e2b125abcfe8cbbe14ffd
JSG1987 <JGentry0058@gmail.com>
2019-09-02 21:55:04 -0500

    This is HW 12 from page 81 redone with a cin statement

---
+++ HW2p12/Source.cpp
@@ -0,0 +1,25 @@
+//The goal of this program is to calculate the acrege of 391,876 square feet using a cin statment
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int acre = 43560;
+	int sqft; //391876;
+
+	float convert = sqft / acre;
+	cout << "Enter the square ft that you want to convert.\n";
+	cin >> sqft;
+	cout << "391,876 square feet is " << convert << " acres\n";
+
+
+
+
+
+
+	system("PAUSE");
+	return 0;
+}
